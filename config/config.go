74c98eed164c8ba6d7c9761ba9844f38200bf01c
SohZHong <sohzhehong09@gmail.com>
2025-03-22 01:37:13 +0800

    add: Google OAuth config

--- config/config.go
+++ config/config.go
@@ -8,16 +8,29 @@ import (
 
 type Config struct {
 	DB    PostgresConfig
+	OAuth GoogleOAuthConfig
 }
 
 type PostgresConfig struct {
 	Url string
 }
 
+type GoogleOAuthConfig struct {
+	ClientID     string
+	ClientSecret string
+	RedirectURL  string
+}
+
+
 func LoadConfig() (*Config, error) {
 	cfg := &Config{
 		DB: PostgresConfig{
 			Url: os.Getenv("POSTGRES_URL"),
+		},
+		OAuth: GoogleOAuthConfig{
+			ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
+			ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
+			RedirectURL:  os.Getenv("GOOGLE_REDIRECT_URL"),
 		},
 	}
 
